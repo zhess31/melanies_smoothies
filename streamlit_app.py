852ab9ec2d53c626948f937d0aff7ac0e4e2f65e
zhess31 <93786265+zhess31@users.noreply.github.com>
2025-02-10 15:27:30 -0500

    Update streamlit_app.py
    adding in extra formatting and adjustments.

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,4 +42,5 @@ if ingredients_list:
 
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json(), use_container_width = True)
+# st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width = True)
